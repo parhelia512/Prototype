2ce81594e3362b12414de0e6024d5ac38bc6ce6c
Tom <tomwer3@gmail.com>
2025-02-15 05:21:55 -0600

    TileAssignmentLayer: Draw the big fat cross and indicate tiles worked by other cities
    #541

--- C7/Map/TileAssignmentLayer.cs
+++ C7/Map/TileAssignmentLayer.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using C7GameData;
 using Godot;
 
@@ -8,18 +9,44 @@ namespace C7.Map {
 		ImageTexture shieldTexture = Util.LoadTextureFromPCX("Art/city screen/CityIcons.pcx", 133, 1, 16, 30);
 		ImageTexture goldTexture = Util.LoadTextureFromPCX("Art/city screen/CityIcons.pcx", 67, 1, 21, 30);
 
+		private const int tileWidth = 128;
+		private const int tileHeight = 64;
+
 		// When non-null, the city whose tile assignments should be shown.
 
 		public City city = null;
+		private HashSet<Tile> workableTiles;
+
+		public override void onBeginDraw(LooseView looseView, GameData gameData) {
+			if (city == null) {
+				return;
+			}
+			workableTiles = city.GetWorkableTiles();
+
+			// Include the city center in the "workable" tiles to avoid having
+			// a border drawn there.
+			workableTiles.Add(city.location);
+		}
 
 		public override void drawObject(LooseView looseView, GameData gameData, Tile tile, Vector2 tileCenter) {
 			if (city == null) {
 				return;
 			}
 
-			// TODO: we probably want to indicate whether a tile is part of the
-			// city's big fat cross (assuming a border expand) but ignoring
-			// unworked tiles should work for now.
+			// Draw a nice border around our city's workable tiles.
+			//
+			// TODO: use the fog of war layer to highlight the BFC.
+			if (workableTiles.Contains(tile)) {
+				DrawWorkableTileBorder(looseView, tile, tileCenter);
+
+				// If one of our workable tiles is worked by another city, draw
+				// a rectangle around it.
+				if (tile.personWorkingTile != null && tile.personWorkingTile.city != city) {
+					DrawOccupiedTileSquare(looseView, tileCenter);
+				}
+			}
+
+			// Only draw yields for our citizens.
 			if (tile.personWorkingTile?.city != city && tile.cityAtTile != city) {
 				return;
 			}
@@ -47,5 +74,73 @@ namespace C7.Map {
 			}
 		}
 
+		private void DrawWorkableTileBorder(LooseView looseView, Tile tile, Vector2 tileCenter) {
+			if (!workableTiles.Contains(tile.neighbors[TileDirection.NORTHWEST])) {
+				looseView.DrawLine(tileCenter + new Vector2(-tileWidth / 2, 0),
+									tileCenter + new Vector2(0, -tileHeight / 2),
+									Colors.White, /*width=*/2);
+			}
+
+			if (!workableTiles.Contains(tile.neighbors[TileDirection.NORTHEAST])) {
+				looseView.DrawLine(tileCenter + new Vector2(tileWidth / 2, 0),
+									tileCenter + new Vector2(0, -tileHeight / 2),
+									Colors.White, /*width=*/2);
+			}
+
+			if (!workableTiles.Contains(tile.neighbors[TileDirection.SOUTHWEST])) {
+				looseView.DrawLine(tileCenter + new Vector2(-tileWidth / 2, 0),
+									tileCenter + new Vector2(0, tileHeight / 2),
+									Colors.White, /*width=*/2);
+			}
+
+			if (!workableTiles.Contains(tile.neighbors[TileDirection.SOUTHEAST])) {
+				looseView.DrawLine(tileCenter + new Vector2(tileWidth / 2, 0),
+									tileCenter + new Vector2(0, tileHeight / 2),
+									Colors.White, /*width=*/2);
+			}
+		}
+
+		private void DrawOccupiedTileSquare(LooseView looseView, Vector2 tileCenter) {
+			// Thick brown outline.
+			{
+				int lineWidth = 4;
+				int width = tileWidth - lineWidth * 4;
+				int height = tileHeight - lineWidth * 2;
+
+				Color brown = new Color(166.0f/256, 116.0f/256, 87.0f/256);
+				DrawSquare(looseView, tileCenter, brown, lineWidth, width, height);
+			}
+
+			// Black borders of the brown outline.
+			{
+				int lineWidth = 1;
+				int width = tileWidth - 4;
+				int height = tileHeight - 2;
+
+				DrawSquare(looseView, tileCenter, Colors.Black, lineWidth, width, height);
+			}
+			{
+				int lineWidth = 1;
+				int width = tileWidth - 20;
+				int height = tileHeight - 10;
+
+				DrawSquare(looseView, tileCenter, Colors.Black, lineWidth, width, height);
+			}
+		}
+
+		private void DrawSquare(LooseView looseView, Vector2 tileCenter, Color color, int lineWidth, int tileWidth, int tileHeight) {
+			looseView.DrawLine(tileCenter + new Vector2(-tileWidth / 2, 0),
+								tileCenter + new Vector2(0, -tileHeight / 2),
+								color, lineWidth);
+			looseView.DrawLine(tileCenter + new Vector2(tileWidth / 2, 0),
+								tileCenter + new Vector2(0, -tileHeight / 2),
+								color, lineWidth);
+			looseView.DrawLine(tileCenter + new Vector2(-tileWidth / 2, 0),
+								tileCenter + new Vector2(0, tileHeight / 2),
+								color, lineWidth);
+			looseView.DrawLine(tileCenter + new Vector2(tileWidth / 2, 0),
+								tileCenter + new Vector2(0, tileHeight / 2),
+								color, lineWidth);
+		}
 	}
 }
