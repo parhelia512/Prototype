785857339a7c4ac6ec169299e54240d8bc005e71
esbudylin <budylin.egor@gmail.com>
2025-03-09 21:07:31 +0400

    SaveGame: refactor building conversion logic
    Building conversion is rewritten with a for loop for better readability

--- C7GameData/Save/SaveGame.cs
+++ C7GameData/Save/SaveGame.cs
@@ -144,24 +144,22 @@ namespace C7GameData.Save {
 		}
 
 		private void ConvertBuildings(GameData data) {
-			var techDict = data.techs.ToDictionary(t => t.id);
-
-			data.Buildings = Buildings.ConvertAll(building =>
-				new Building(
-					building,
-					building.requiredTech != null ? techDict[building.requiredTech] : null
-				)
-			);
+			data.Buildings = Buildings.ConvertAll(building => new Building(building));
 
 			var buildingDict = data.Buildings.ToDictionary(b => b.name);
+			var techDict = data.techs.ToDictionary(t => t.id);
 
-			data.Buildings = data.Buildings
-				.Zip(Buildings, (building, saveBuilding) => {
-					if (saveBuilding.requiredBuilding != null)
-						building.requiredBuilding = buildingDict[saveBuilding.requiredBuilding];
-					return building;
-				})
-				.ToList();
+			foreach (SaveBuilding saveBuilding in Buildings) {
+				Building building = buildingDict[saveBuilding.name];
+
+				if (saveBuilding.requiredBuilding != null) {
+					building.requiredBuilding = buildingDict[saveBuilding.requiredBuilding];
+				}
+
+				if (saveBuilding.requiredTech != null) {
+					building.requiredTech = techDict[saveBuilding.requiredTech];
+				}
+			}
 		}
 
 		private void ConvertUnits(GameData data) {
