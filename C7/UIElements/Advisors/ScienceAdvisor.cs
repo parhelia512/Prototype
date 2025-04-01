13f82dc657fa577f385d056ffa688c374fc68eec
Tom <tomwer3@gmail.com>
2025-03-31 21:24:31 -0500

    Handle era advancement and allow viewing other era in the advisor

--- C7/UIElements/Advisors/ScienceAdvisor.cs
+++ C7/UIElements/Advisors/ScienceAdvisor.cs
@@ -9,17 +9,25 @@ public partial class ScienceAdvisor : TextureRect {
 	private ImageTexture MiddleBackground;
 	private ImageTexture IndustrialBackground;
 	private ImageTexture ModernBackground;
+	private TextureButton nextEra;
+	private TextureButton previousEra;
+	private List<TechBox> techBoxes = new();
+
+	// Stored separately so we can modify this without mutating the player.
+	private string eraName;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready() {
 		this.CreateUI();
-		this.DrawTechTree();
+
+		using UIGameDataAccess gameDataAccess = new();
+		List<Tech> techs = gameDataAccess.gameData.techs;
+		Player player = gameDataAccess.gameData.GetHumanPlayers()[0];
+		eraName = player.eraCivilopediaName;
+		this.DrawTechTree(eraName, player, techs);
 	}
 
 	private void CreateUI() {
-		// TODO: support other eras. Amusingly the dependency arrows are drawn
-		// on this texture, so to render prereqs we need to update this per era.
-		//
 		// science_industrial_new is used as the industrial tech tree is
 		// different from vanilla civ3.
 		AncientBackground = Util.LoadTextureFromPCX("Art/Advisors/science_ancient.pcx");
@@ -59,54 +67,105 @@ public partial class ScienceAdvisor : TextureRect {
 		GoBackButton.SetPosition(new Vector2(952, 720));
 		AddChild(GoBackButton);
 		GoBackButton.Pressed += ReturnToMenu;
+
+		previousEra = new();
+		previousEra.TextureNormal = Util.LoadTextureFromPCX("Art/Tech Chooser/scienceNAV.pcx", 0, 1, 129, 33);
+		previousEra.TextureHover = Util.LoadTextureFromPCX("Art/Tech Chooser/scienceNAV.pcx", 0, 35, 129, 33);
+		previousEra.TexturePressed = Util.LoadTextureFromPCX("Art/Tech Chooser/scienceNAV.pcx", 0, 69, 129, 33);
+		previousEra.SetPosition(new Vector2(512 - 128 - 100, 720));
+		AddChild(previousEra);
+		previousEra.Pressed += () => { ChangeEraAndDrawTree(-1); };
+
+		TextureRect leftArrow = new();
+		leftArrow.Texture = Util.LoadTextureFromPCX("Art/Tech Chooser/scienceNAV.pcx", 0, 103, 44, 9);
+		previousEra.AddChild(leftArrow);
+		leftArrow.SetPosition(new Vector2(-44, 13));
+
+		Label previousEraLabel = new();
+		previousEra.AddChild(previousEraLabel);
+		previousEraLabel.SetTextAndCenterLabel("Previous Era");
+		previousEraLabel.Position += new Vector2(0, 7);
+
+		nextEra = new();
+		nextEra.TextureNormal = Util.LoadTextureFromPCX("Art/Tech Chooser/scienceNAV.pcx", 0, 1, 129, 33);
+		nextEra.TextureHover = Util.LoadTextureFromPCX("Art/Tech Chooser/scienceNAV.pcx", 0, 35, 129, 33);
+		nextEra.TexturePressed = Util.LoadTextureFromPCX("Art/Tech Chooser/scienceNAV.pcx", 0, 69, 129, 33);
+		nextEra.SetPosition(new Vector2(512 + 100, 720));
+		AddChild(nextEra);
+		nextEra.Pressed += () => { ChangeEraAndDrawTree(1); };
+
+		TextureRect rightArrow = new();
+		rightArrow.Texture = Util.LoadTextureFromPCX("Art/Tech Chooser/scienceNAV.pcx", 46, 103, 44, 9);
+		nextEra.AddChild(rightArrow);
+		rightArrow.SetPosition(new Vector2(129, 13));
+
+		Label nextEraLabel = new();
+		nextEra.AddChild(nextEraLabel);
+		nextEraLabel.SetTextAndCenterLabel("Next Era");
+		nextEraLabel.Position += new Vector2(0, 7);
 	}
 
-	void DrawTechTree() {
-		using (UIGameDataAccess gameDataAccess = new()) {
-			List<Tech> techs = gameDataAccess.gameData.techs;
-			Player player = gameDataAccess.gameData.GetHumanPlayers()[0];
-			HashSet<ID> knownTechs = player.knownTechs;
+	void DrawTechTree(string eraName, Player player, List<Tech> allTechs) {
+		HashSet<ID> knownTechs = player.knownTechs;
+		previousEra.Show();
+		nextEra.Show();
 
-			// Set the tech background based on the player's era.
-			if (player.eraCivilopediaName == "ERAS_Ancient_Times") {
-				this.Texture = AncientBackground;
-			} else if (player.eraCivilopediaName == "ERAS_Middle_Ages") {
-				this.Texture = MiddleBackground;
-			} else if (player.eraCivilopediaName == "ERAS_Industrial_Age") {
-				this.Texture = IndustrialBackground;
-			} else if (player.eraCivilopediaName == "ERAS_Modern_Era") {
-				this.Texture = ModernBackground;
-			}
-
-			foreach (Tech tech in techs) {
-				if (tech.EraCivilopediaName != player.eraCivilopediaName) {
-					continue;
-				}
-
-				TechBox.TechState techState = TechBox.TechState.kBlocked;
-				if (knownTechs.Contains(tech.id)) {
-					techState = TechBox.TechState.kKnown;
-				} else if (player.currentlyResearchedTech == tech.id) {
-					techState = TechBox.TechState.kInProgress;
-				} else {
-					bool prereqsKnown = true;
-					foreach (Tech prereq in tech.Prerequisites) {
-						if (!knownTechs.Contains(prereq.id)) {
-							prereqsKnown = false;
-							break;
-						}
-					}
-					techState = prereqsKnown ? TechBox.TechState.kPossible : TechBox.TechState.kBlocked;
-				}
-
-				TechBox techButton = new(tech, techState);
-				techButton.SetPosition(new Vector2(tech.X, tech.Y));
-				techButton.Pressed += () => {
-					new MsgChooseResearch(tech.id).send();
-				};
-				AddChild(techButton);
-			}
+		// Set the tech background based on the player's era.
+		if (eraName == "ERAS_Ancient_Times") {
+			previousEra.Hide();
+			this.Texture = AncientBackground;
+		} else if (eraName == "ERAS_Middle_Ages") {
+			this.Texture = MiddleBackground;
+		} else if (eraName == "ERAS_Industrial_Age") {
+			this.Texture = IndustrialBackground;
+		} else if (eraName == "ERAS_Modern_Era") {
+			this.Texture = ModernBackground;
+			nextEra.Hide();
 		}
+
+		foreach (Tech tech in allTechs) {
+			if (tech.EraCivilopediaName != eraName) {
+				continue;
+			}
+
+			TechBox.TechState techState = TechBox.TechState.kBlocked;
+			if (knownTechs.Contains(tech.id)) {
+				techState = TechBox.TechState.kKnown;
+			} else if (player.currentlyResearchedTech == tech.id) {
+				techState = TechBox.TechState.kInProgress;
+			} else {
+				bool prereqsKnown = true;
+				foreach (Tech prereq in tech.Prerequisites) {
+					if (!knownTechs.Contains(prereq.id)) {
+						prereqsKnown = false;
+						break;
+					}
+				}
+				techState = prereqsKnown ? TechBox.TechState.kPossible : TechBox.TechState.kBlocked;
+			}
+
+			TechBox techButton = new(tech, techState);
+			techButton.SetPosition(new Vector2(tech.X, tech.Y));
+			techButton.Pressed += () => {
+				new MsgChooseResearch(tech.id).send();
+			};
+			AddChild(techButton);
+			techBoxes.Add(techButton);
+		}
+	}
+
+	private void ChangeEraAndDrawTree(int delta) {
+		foreach (TechBox tb in techBoxes) {
+			RemoveChild(tb);
+			tb.QueueFree();
+		}
+		techBoxes.Clear();
+
+		using UIGameDataAccess gameDataAccess = new();
+		List<Tech> techs = gameDataAccess.gameData.techs;
+		Player player = gameDataAccess.gameData.GetHumanPlayers()[0];
+		eraName = Player.EraIndexToEra(Player.EraIndex(eraName) + delta);
+		DrawTechTree(eraName, player, techs);
 	}
 
 	private void ReturnToMenu() {
