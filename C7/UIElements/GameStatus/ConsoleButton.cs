ef530249064b2347f3e4277fd2a1c95cc311f3a7
esbudylin <budylin.egor@gmail.com>
2025-04-07 11:49:10 +0400

    Minor refactor

--- C7/UIElements/GameStatus/ConsoleButton.cs
+++ C7/UIElements/GameStatus/ConsoleButton.cs
@@ -22,18 +22,20 @@ public partial class ConsoleButton : Control {
 	private TextureButton button;
 
 	public override void _Ready() {
-		button = new();
-		button.TextureNormal = Util.LoadTextureFromPCX("Art/interface/consoleButtons.pcx", 1, 1, 16, 16);
-		button.TextureHover = Util.LoadTextureFromPCX("Art/interface/consoleButtons.pcx", 17, 1, 16, 16);
-		button.TexturePressed = Util.LoadTextureFromPCX("Art/interface/consoleButtons.pcx", 33, 1, 16, 16);
-		button.TooltipText = tooltipText;
+		button = new() {
+			TextureNormal = Util.LoadTextureFromPCX("Art/interface/consoleButtons.pcx", 1, 1, 16, 16),
+			TextureHover = Util.LoadTextureFromPCX("Art/interface/consoleButtons.pcx", 17, 1, 16, 16),
+			TexturePressed = Util.LoadTextureFromPCX("Art/interface/consoleButtons.pcx", 33, 1, 16, 16),
+			TooltipText = tooltipText
+		};
 		button.Pressed += () => { EmitSignal(SignalName.Pressed); };
 
-		Label label = new();
-		label.Text = text;
-		label.OffsetLeft = 3;
-		label.OffsetTop = -3;
-		label.MouseFilter = Control.MouseFilterEnum.Ignore;
+		Label label = new() {
+			Text = text,
+			OffsetLeft = 3,
+			OffsetTop = -3,
+			MouseFilter = MouseFilterEnum.Ignore
+		};
 
 		AddChild(button);
 		button.AddChild(label);
--- C7/UIElements/GameStatus/StatusMenu.cs
+++ C7/UIElements/GameStatus/StatusMenu.cs
@@ -17,18 +17,17 @@ public partial class StatusMenu : Control {
 	}
 
 	public override void _Process(double delta) {
-		using (UIGameDataAccess gameDataAccess = new()) {
-			GameData gD = gameDataAccess.gameData;
-			Player player = gD.GetHumanPlayers()[0];
+		using UIGameDataAccess gameDataAccess = new();
+		GameData gD = gameDataAccess.gameData;
+		Player player = gD.GetHumanPlayers()[0];
 
-			// Only show the diplomacy button if we have civs to talk to.
-			if (player.playerRelationships.Count > 0) {
-				openDiplomacy.ShowButton();
-			}
-
-			// TODO: Don't show the palace button if the player can't start building the palace
-			openPalaceScreen.ShowButton();
+		// Only show the diplomacy button if we have civs to talk to.
+		if (player.playerRelationships.Count > 0) {
+			openDiplomacy.ShowButton();
 		}
+
+		// TODO: Don't show the palace button if the player can't start building the palace
+		openPalaceScreen.ShowButton();
 	}
 
 	private void OpenDiplomacyPopup() {
