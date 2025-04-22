0067dbacbad1aa456f57de706dece5d72cc9a4f4
Tom <tomwer3@gmail.com>
2025-04-22 11:58:47 -0500

    Centralize the logic for rendering popheads
    I'll reuse this logic in the domestic advisor

---
+++ C7/UIElements/CityScreen/PopHead.cs
@@ -0,0 +1,42 @@
+using Godot;
+using System;
+using C7GameData;
+
+// A utility class for rendering pop heads.
+public class PopHeads {
+	public const int HEAD_SIZE = 48;
+	private const int HEAD_SIZE_WITH_BORDER = 50;
+	private const int NUM_ERAS = 4;
+	private const int MOODS_PER_ERA = 4;
+
+	public static ImageTexture GetPopHead(CityResident cr, int eraNum) {
+		if (cr.citizenType.IsDefaultCitizen) {
+			return GetLaborerPopHead(cr, eraNum);
+		}
+		return GetSpecialistPopHead(cr, eraNum);
+	}
+
+	private static ImageTexture GetSpecialistPopHead(CityResident cr, int eraNum) {
+		int X = HEAD_SIZE_WITH_BORDER * eraNum;
+		int numRowsOfLaborers = NUM_ERAS * MOODS_PER_ERA;
+		int Y = HEAD_SIZE_WITH_BORDER * numRowsOfLaborers
+				 + HEAD_SIZE_WITH_BORDER * (cr.citizenType.SpecialistIndex - 1);
+		return Util.LoadTextureFromPCX("Art/SmallHeads/popHeads.pcx",
+										X + 1, Y + 1, HEAD_SIZE, HEAD_SIZE);
+	}
+
+	private static ImageTexture GetLaborerPopHead(CityResident cr, int eraNum) {
+		int column = cr.mood switch {
+			CityResident.Mood.Content => 0,
+			CityResident.Mood.Happy => 1,
+			CityResident.Mood.Unhappy => 3,
+		};
+		int headWithBorderSize = HEAD_SIZE + 2;
+
+		return Util.LoadTextureFromPCX("Art/SmallHeads/popHeads.pcx",
+										0 + 1,
+										MOODS_PER_ERA * HEAD_SIZE_WITH_BORDER * eraNum
+											+ HEAD_SIZE_WITH_BORDER * column + 1,
+										HEAD_SIZE, HEAD_SIZE);
+	}
+}
