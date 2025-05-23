85b799c85132ccbf8b9a7e70ab54388feaae6ab4
esbudylin <budylin.egor@gmail.com>
2025-05-23 19:05:11 +0400

    Display luxury icons in the city screen

--- C7/Text/TextureConfigs/civ3/resources.lua
+++ C7/Text/TextureConfigs/civ3/resources.lua
@@ -1,12 +1,15 @@
 local RESOURCE_SIZE = 50
+local LUXURY_SMALL_SIZE = 22
 
-local resources = {
+local resources = {}
+
+resources.large = {
   extra_data = {
     path = "Art/resources.pcx",
   },
 }
 
-function resources:map_object_to_sprite(resource)
+function resources.large:map_object_to_sprite(resource)
   if resource:GetType().Name ~= "Resource" then
     error "Expected a Resource object"
   end
@@ -18,6 +21,31 @@ function resources:map_object_to_sprite(resource)
   return {
     path = self.extra_data.path,
     crop_region = { col * RESOURCE_SIZE, row * RESOURCE_SIZE, RESOURCE_SIZE, RESOURCE_SIZE },
+    shadows = false,
+  }
+end
+
+resources.small = {
+  extra_data = {
+    path = "Art/city screen/luxuryicons_small.pcx",
+  },
+}
+
+function resources.small:map_object_to_sprite(resource)
+  if resource:GetType().Name ~= "Resource" then
+    error "Expected a Resource object"
+  end
+
+  local icon = resource.Icon
+  local col = icon - 8
+
+  if col < 0 then
+    error "Invalid resource icon index"
+  end
+
+  return {
+    path = self.extra_data.path,
+    crop_region = { col * LUXURY_SMALL_SIZE, 0, LUXURY_SMALL_SIZE, LUXURY_SMALL_SIZE },
     shadows = false,
   }
 end
