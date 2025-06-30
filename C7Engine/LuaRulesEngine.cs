5d57b8e4c450f76f604056d84c978c9883e7219e
esbudylin <budylin.egor@gmail.com>
2025-06-30 11:57:26 +0400

    Code comments

--- C7Engine/LuaRulesEngine.cs
+++ C7Engine/LuaRulesEngine.cs
@@ -74,14 +74,32 @@ public static class LuaDelegateConverter {
 	}
 }
 
+/// A rules engine that loads game behaviors defined in a Lua script.
+///
+/// Works with Lua scripts that return a nested table where the leaves
+/// are functions.
+///
+/// The intended use case is during the game initialization (e.g.,
+/// when loading a JSON save), to transform given Lua function paths
+/// into C# delegates.
+///
+/// When initializing the Lua state, this class registers all public
+/// types in the C7GameData namespace as Moonsharp userdata, which
+/// exposes them to the scripting engine.
+///
+/// The engine registers the following Lua globals:
+/// - GAME_DATA(): Function returning the current game data instance.
+/// - ENUMS:  Table containing definitions of public enums from the C7GameData namespace.
+///   Note that nested enums are registered under a name concatenating the outer class and the enum names (e.g., Tile_YieldType)
 public class LuaRulesEngine {
 	static ILogger log = Log.ForContext<LuaRulesEngine>();
 	Script script = new();
 	Table rules;
 
 	/// Initializes the Lua state.
+	///
 	/// Accepts the path to the directory containing Lua scripts,
-	/// and the name of the main script that should return a rules table.
+	/// and the name of the script that should return a rules table.
 	public void Initialize(string luaRulesDir, string rulesScript) {
 		if (rules != null)
 			throw new InvalidOperationException("Engine already initialized");
@@ -97,6 +115,12 @@ public class LuaRulesEngine {
 		rules = res.Table;
 	}
 
+	/// Loads a Lua function from a rules table by a given path and
+	/// transforms it into C# delegate of the given generic type.
+	///
+	/// The path to the function should be a dot-separated string
+	/// representing the path through the rules table
+	/// (e.g. "building.production_rules.must_be_coastal")
 	public T ImportFunc<T>(string functionPath) where T : Delegate {
 		if (script == null || rules == null)
 			throw new InvalidOperationException("Engine is not initialized");
@@ -148,7 +172,9 @@ public class LuaRulesEngine {
 								t.Namespace == "C7GameData");
 
 		Table enumTable = new(script);
+
 		foreach (var enumType in enumTypes) {
+			// For nested enums, concatenate the outer class name with the enum name
 			string registrationName = enumType.DeclaringType != null
 			? $"{enumType.DeclaringType.Name}_{enumType.Name}"  // e.g., Tile_YieldType
             : enumType.Name;
