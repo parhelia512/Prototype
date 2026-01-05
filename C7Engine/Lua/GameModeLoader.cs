c8d59401635c0420ae545e13c90a645c7ed346f5
esbudylin <budylin.egor@gmail.com>
2026-01-05 12:35:25 +0400

    Code comments

--- C7Engine/Lua/GameModeLoader.cs
+++ C7Engine/Lua/GameModeLoader.cs
@@ -8,7 +8,12 @@ using MoonSharp.Interpreter;
 namespace C7Engine.Lua;
 
 public class GameModeConfig {
+	// The base scenario definition file. Should be a JSON or a Lua
+	// script returning a table
 	public string baseModePath;
+
+	// Optional addon Lua scripts. Each script should return a
+	// function that modifies the scenario data and returns it.
 	public List<string> addonPaths = [];
 
 	public GameModeConfig(string baseModePath, List<string> addonPaths = null) {
@@ -17,6 +22,7 @@ public class GameModeConfig {
 	}
 }
 
+// Loads OpenCiv3 scenario definitions
 public class GameModeLoader {
 	private static ILogger log = Log.ForContext<GameModeLoader>();
 
@@ -29,12 +35,14 @@ public class GameModeLoader {
 		string fullBaseModePath = Path.Combine(gameModesDir, baseModePath);
 		log.Information("Loading base game mode file: {baseModePath}", fullBaseModePath);
 
+		// Load base scenario from JSON or Lua file
 		DynValue current = Path.GetExtension(fullBaseModePath).ToUpper() switch {
 			".JSON" => converter.Decode(File.ReadAllText(fullBaseModePath)),
 			".LUA" => lua.SafeDoFile(fullBaseModePath),
 			_ => throw new InvalidOperationException("Invalid base mode file format")
 		};
 
+		// Process through addon scripts
 		foreach (string addonPath in config.addonPaths) {
 			log.Information("Loading addon: {addonPath}", addonPath);
 
@@ -45,9 +53,11 @@ public class GameModeLoader {
 					$"Addon '{addonPath}' must return a function"
 				);
 
+			// Each addon function modifies and returns the scenario data
 			current = lua.SafeCall(addon.Function, current);
 		}
 
+		// Convert final Lua table to JSON for deserialization
 		string json = converter.Encode(current);
 
 		return SaveGame.LoadFromJSON(json);
