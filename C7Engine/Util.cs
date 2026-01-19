7d89418e1bf21a566525e4c892982578d2eee32b
Stavros <farmakis.stavros@gmail.com>
2026-01-19 19:25:35 +0200

    Address review comments

--- C7Engine/Util.cs
+++ C7Engine/Util.cs
@@ -9,7 +9,11 @@ public static class Util {
 		if (fileIniDataParser != null) return fileIniDataParser;
 
 		FileIniDataParser parser = new FileIniDataParser();
+		// The default behaviour of the parser is to throw an exception
+		// when it finds duplicate keys (e.x. Tank.ini has 'DEAD' two times in [Sound Effects])
+		// so, we want to allow it so that it doesn't crash
 		parser.Parser.Configuration.AllowDuplicateKeys = true;
+		// but we only want to keep the last value we encounter
 		parser.Parser.Configuration.OverrideDuplicateKeys = true;
 
 		fileIniDataParser = parser;
