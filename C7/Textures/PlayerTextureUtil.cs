138a4eb12091ef2d709a48e9a03f688b8fdaeb9f
stavrosfa <67387767+stavrosfa@users.noreply.github.com>
2026-02-17 01:22:25 +0200

    Formatting

--- C7/Textures/PlayerTextureUtil.cs
+++ C7/Textures/PlayerTextureUtil.cs
@@ -7,16 +7,16 @@ using Godot;
 namespace C7.Textures;
 
 public static class PlayerTextureUtil {
-    // <colorIndex, shaderMaterial>
+	// <colorIndex, shaderMaterial>
 	private static Dictionary<int, ShaderMaterial> materialCache = [];
-    // <playerId, colorIndex>
+	// <playerId, colorIndex>
 	private static Dictionary<ID, int> colorsInUseCache = [];
 
-    /// <summary>
-    /// Returns the color index this player will use for the game, for their border, uniform, etc
-    /// </summary>
-    /// <param name="player"></param>
-    /// <returns>the color index number</returns>
+	/// <summary>
+	/// Returns the color index this player will use for the game, for their border, uniform, etc
+	/// </summary>
+	/// <param name="player"></param>
+	/// <returns>the color index number</returns>
 	public static int GetPlayerColor(this Player player) {
 		if (!colorsInUseCache.ContainsKey(player.id)) {
 			InitializeCivColors(EngineStorage.gameData);
@@ -24,22 +24,22 @@ public static class PlayerTextureUtil {
 		return colorsInUseCache[player.id];
 	}
 
-    // If the player's primary color is in use, fall back to their secondary color.
-    // This may already be in use, but that's ok; we don't have a third
-    // color for each civ to fall back to.
-    //
-    // If this runs for a second time from InitializeCivColors()
-    // we want to return if possible a color that is not already in use.
-    // Consider a game where the Netherlands and England are in play.
-    // Netherlands' color indexes are 2, 18
-    // England's color indexes    are 2, 2
-    // If the player is playing as England, we will pick 2 either way.
-    // Then, Netherlands will pick 18, because 2 is in use.
-    // But if the player is playing as the Netherlands, we will pick 2, because the player's civ is first on the list.
-    // Then, when England's turn comes, we try the primary 2, it's in use, so we pick the secondary 2.
-    // After all player are assigned at least one color, we check for duplicates in the colorsInUseCache values.
-    // Netherlands is the first player we check, we find that their color, 2, is a duplicate, 
-    // so we assign the secondary color 18 to the Netherlands, and England keep their 2.
+	// If the player's primary color is in use, fall back to their secondary color.
+	// This may already be in use, but that's ok; we don't have a third
+	// color for each civ to fall back to.
+	//
+	// If this runs for a second time from InitializeCivColors()
+	// we want to return if possible a color that is not already in use.
+	// Consider a game where the Netherlands and England are in play.
+	// Netherlands' color indexes are 2, 18
+	// England's color indexes    are 2, 2
+	// If the player is playing as England, we will pick 2 either way.
+	// Then, Netherlands will pick 18, because 2 is in use.
+	// But if the player is playing as the Netherlands, we will pick 2, because the player's civ is first on the list.
+	// Then, when England's turn comes, we try the primary 2, it's in use, so we pick the secondary 2.
+	// After all player are assigned at least one color, we check for duplicates in the colorsInUseCache values.
+	// Netherlands is the first player we check, we find that their color, 2, is a duplicate, 
+	// so we assign the secondary color 18 to the Netherlands, and England keep their 2.
 	private static int LoadCivColor(Player player) {
 		if (colorsInUseCache.TryGetValue(player.id, out int colorIndex))
 			return colorIndex;
