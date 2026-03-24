447f5cfe654c1553d7af5de013058cd79c013fb6
Antti Halme <antti.halme@iki.fi>
2026-03-24 00:45:20 +0000

    Draw bounds across the map edges

--- C7/UIElements/MiniMap/MiniMapControls.cs
+++ C7/UIElements/MiniMap/MiniMapControls.cs
@@ -11,17 +11,85 @@ public partial class MiniMapControls : Control {
 	public void DrawBounds(Image mapImage, GameMap map, MapView.VisibleRegion vr) {
 		// TODO: Handle draws over map edges, maybe with Mathf.Wrap
 
-		// Clamp coordinates to draw space
-		var ax = Math.Clamp(vr.upperLeftX, 0, map.numTilesWide - 2);
-		var ay = Math.Clamp(vr.upperLeftY / 2, 0, (map.numTilesTall - 1) / 2);
-		var bx = Math.Clamp(vr.lowerRightX, 0, map.numTilesWide - 2);
-		var by = Math.Clamp(vr.lowerRightY / 2, 0, (map.numTilesTall - 1) / 2);
+		// Bounds, in minimap draw space
+		var maxWidth = map.numTilesWide - 1;
+		var maxHeight = (map.numTilesTall / 2) - 1;
+		var maxPan = 100; // how many screenfuls one can pan the map
 
-		// Render
-		DrawLineOnImage(mapImage, ax, ay, ax, by, Colors.White);
-		DrawLineOnImage(mapImage, ax, by, bx, by, Colors.White);
-		DrawLineOnImage(mapImage, bx, by, bx, ay, Colors.White);
-		DrawLineOnImage(mapImage, bx, ay, ax, ay, Colors.White);
+		// Wrapped coordinates, working around modulo operator limitations
+		var wax = (maxPan * maxWidth + vr.upperLeftX) % maxWidth;
+		var way = (maxPan * maxHeight + (vr.upperLeftY / 2)) % maxHeight;
+		var wbx = (maxPan * maxWidth + vr.lowerRightX) % maxWidth;
+		var wby = (maxPan * maxHeight + (vr.lowerRightY / 2)) % maxHeight;
+
+		// Out of bounds
+		var isOoBx = wbx < wax; // X coordinates increase right
+		var isOoBy = wby < way; // Y coordinates increase down
+		var fullZoom = (vr.lowerRightY / 2 - vr.upperLeftY / 2) > maxHeight
+					   || (vr.lowerRightX - vr.upperLeftX) > maxWidth;
+
+		// TODO: make use of GameMap's wrapHorizontally, wrapVertically
+
+		// Handle the four cases
+		if (fullZoom)
+			DrawBoundsRegular(mapImage, 0, 0, maxWidth, maxHeight);
+		else if (isOoBx && isOoBy)
+			DrawBoundsOverCorner(mapImage, wax, way, wbx, wby, maxWidth, maxHeight);
+		else if (isOoBx)
+			DrawBoundsOverSideEdges(mapImage, wax, way, wbx, wby, maxWidth, maxHeight);
+		else if (isOoBy)
+			DrawBoundsOverPoles(mapImage, wax, way, wbx, wby, maxWidth, maxHeight);
+		else
+			DrawBoundsRegular(mapImage, wax, way, wbx, wby);
+	}
+
+	private void DrawBoundsOverCorner(Image mapImage, int wax, int way, int wbx, int wby, int maxWidth, int maxHeight) {
+		// Top Left
+		DrawLineOnImage(mapImage, 0, wby, wbx, wby, Colors.White); // bottom
+		DrawLineOnImage(mapImage, wbx, wby, wbx, 0, Colors.White); // right
+
+		// Top Right
+		DrawLineOnImage(mapImage, wax, 0, wax, wby, Colors.White); // left
+		DrawLineOnImage(mapImage, wax, wby, maxWidth, wby, Colors.White); // bottom
+
+		// Bottom Left 
+		DrawLineOnImage(mapImage, 0, way, wbx, way, Colors.White); // top
+		DrawLineOnImage(mapImage, wbx, way, wbx, maxHeight, Colors.White); // right
+
+		// Bottom Right
+		DrawLineOnImage(mapImage, wax, maxHeight, wax, way, Colors.White); // left
+		DrawLineOnImage(mapImage, wax, way, maxWidth, way, Colors.White); // top
+	}
+
+	private void DrawBoundsOverSideEdges(Image mapImage, int wax, int way, int wbx, int wby, int maxWidth, int maxHeight) {
+		// Left half
+		DrawLineOnImage(mapImage, 0, way, wbx, way, Colors.White); // top
+		DrawLineOnImage(mapImage, wbx, way, wbx, wby, Colors.White); // right
+		DrawLineOnImage(mapImage, 0, wby, wbx, wby, Colors.White); // bottom
+
+		// Right half
+		DrawLineOnImage(mapImage, maxWidth, way, wax, way, Colors.White); // top
+		DrawLineOnImage(mapImage, wax, way, wax, wby, Colors.White); // left
+		DrawLineOnImage(mapImage, wax, wby, maxWidth, wby, Colors.White); // bottom		
+	}
+
+	private void DrawBoundsOverPoles(Image mapImage, int wax, int way, int wbx, int wby, int maxWidth, int maxHeight) {
+		// Top half
+		DrawLineOnImage(mapImage, wax, 0, wax, wby, Colors.White); // left
+		DrawLineOnImage(mapImage, wax, wby, wbx, wby, Colors.White); // bottom
+		DrawLineOnImage(mapImage, wbx, wby, wbx, 0, Colors.White); // right
+
+		// Bottom half
+		DrawLineOnImage(mapImage, wax, maxHeight, wax, way, Colors.White); // left
+		DrawLineOnImage(mapImage, wax, way, wbx, way, Colors.White); // top
+		DrawLineOnImage(mapImage, wbx, way, wbx, maxHeight, Colors.White); // right
+	}
+
+	private void DrawBoundsRegular(Image mapImage, int wax, int way, int wbx, int wby) {
+		DrawLineOnImage(mapImage, wax, way, wax, wby, Colors.White); // left
+		DrawLineOnImage(mapImage, wax, wby, wbx, wby, Colors.White); // bottom
+		DrawLineOnImage(mapImage, wbx, wby, wbx, way, Colors.White); // right
+		DrawLineOnImage(mapImage, wbx, way, wax, way, Colors.White); // top
 	}
 
 	/// <summary>
