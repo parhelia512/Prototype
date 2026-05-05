806e8294f9c1bfcf62415678abb9da078735c5a9
stavrosfa <67387767+stavrosfa@users.noreply.github.com>
2026-05-06 01:13:52 +0300

    Simple unit test for amb Reader

---
+++ EngineTests/GameData/AmbReaderTest.cs
@@ -0,0 +1,20 @@
+using System.IO;
+using System.Linq;
+using ConvertCiv3Media;
+using QueryCiv3;
+using Xunit;
+
+namespace EngineTests.GameData;
+
+public class AmbReaderTest {
+	[Fact]
+	public void WorkerRunAmbTest() {
+		string path = Path.Combine(Civ3Location.GetCiv3Path(), "Art", "Units", "Worker", "WorkerRun.amb");
+
+		Sfx sfx1 = new Amb(path).soundEffects.First();
+		Sfx sfx2 = new Amb(path).soundEffects.Skip(1).First();
+
+		Assert.Equal("WorkRunFoot1.wav", sfx1.wavName);
+		Assert.Equal("WorkRunFoot2.wav", sfx2.wavName);
+	}
+}
